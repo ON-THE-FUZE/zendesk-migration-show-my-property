60efd8f62823ee89431dd195551b058e453e0930
dquirogat-otf <daniel.quirogatorres@onthefuze.com>
2023-12-28 21:54:21 -0500

    Creation of the loggin object

---
+++ src/global/logger/pino.js
@@ -0,0 +1,33 @@
+import { join } from 'desm';
+import pino from 'pino';
+
+/**
+ * TODO: Update all the paths that you need to save the logs on the migration
+ *
+ * **/
+const errorPath = join(import.meta.url, './logs/error.mjs');
+const debugPath = join(import.meta.url, './logs/debug.mjs');
+const successPath = join(import.meta.url, './logs/success.mjs');
+
+const transport = pino.transport({
+  targets: [
+    {
+      level: 'info',
+      target: 'pino/file',
+      options: { destination: successPath },
+    },
+    {
+      level: 'error',
+      target: 'pino/file',
+      options: { destination: errorPath },
+    },
+    {
+      target: 'pino-pretty',
+      options: { destination: 1 }, // use 2 for stderr
+    },
+  ],
+});
+
+const logger = pino(transport);
+
+export { logger };
